3a28978ee6e141a5ecbd69d5e1763bf473a9fb92
pctiope <pctiope@up.edu.ph>
2023-05-19 10:55:58 +0800

    testing if max poly is a poly

--- ph_random.py
+++ ph_random.py
@@ -5,9 +5,9 @@ from shapely.ops import nearest_points
 def random_waypoints(poly, max_poly):
      minx, miny, maxx, maxy = poly.bounds
      if len(max_poly[0][0]) > 0:
-         print(max_poly)
-         #mp_coords = None
-         mp_coords = list(Polygon(zip(max_poly[0][0])).exterior.coords)
+         print(Polygon(max_poly[0][0]))
+         mp_coords = None
+         #mp_coords = list(Polygon(zip(max_poly[0][0])).exterior.coords)
      else:
          mp_coords = None
      while True:
