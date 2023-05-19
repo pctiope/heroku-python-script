382e1a9836476bb4c57ef513527da8a1cf07973d
pctiope <pctiope@up.edu.ph>
2023-05-19 11:30:51 +0800

    set dt to point

--- ph_random.py
+++ ph_random.py
@@ -15,9 +15,9 @@ def random_waypoints(poly, max_poly):
          mp_coords = None
      while True:
          if mp_coords != None:
-            mp = random.choice(mp_coords)
+            mp = Point(random.choice(mp_coords))
             np_coords = list(nearest_points(poly, mp))
-            np = random.choice(np_coords).wkt
+            np = Point(random.choice(np_coords))
          else:
             np = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
          p = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
