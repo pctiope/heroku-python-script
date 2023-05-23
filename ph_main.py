8edb07d1f6d8c8e270408b63021e3b78e45c61ac
pctiope <pctiope@up.edu.ph>
2023-05-23 23:02:48 +0800

    for testing

--- ph_main.py
+++ ph_main.py
@@ -9,8 +9,10 @@ from ph_polygonize import polygonize
 from ph_filter import filter
 from ph_routing import generate_route
 from ph_random import random_waypoints
-from ph_normal import generate_normal
 import random
+
+with open("./metro_manila.geojson") as f:
+        data = json.load(f)
 
 class Sensor:
      def __init__(self,name,x,y,aqi):
@@ -18,9 +20,6 @@ class Sensor:
         self.x = x
         self.y = y
         self.aqi = aqi
-
-with open("./metro_manila.geojson") as f:
-        data = json.load(f)
 
 while 1:
     format = "%d-%m-%Y_%H-%M-%S"        # dd-mm-yyyy_HH-MM-SS format
@@ -30,36 +29,36 @@ while 1:
     df_to_csv(df, date_time)
     df_to_shp(df, date_time)
     get_idw(date_time)
-    
     max_AQI = max([int(i) for i in US_AQI])
     threshold = max_AQI
     print("threshold: "+str(threshold))
     polygonize(threshold, date_time)
-    max_poly = filter(threshold, date_time)
-    #print(max_poly)
-    
+    exclude_poly = filter(threshold, date_time)
+    print(exclude_poly)
     poly = Polygon(data['features'][0]['geometry']['coordinates'][0][0])
+    
     sensors = []
     for i in range(len(Sensor_Name)):
          sensors.append(Sensor(Sensor_Name[i],X_location[i],Y_location[i],US_AQI[i]))
     sensors = sorted(sensors, key=lambda x: x.aqi, reverse=True)
-    top5_rand = random.randint(0,0)
+    top5_rand = random.randint(0,2)
+    print(sensors[top5_rand].x,sensors[top5_rand].y)
     first_point, second_point = random_waypoints(poly, sensors[0].x, sensors[0].y)
 
     coords = [[first_point.x, first_point.y], [second_point.x, second_point.y]]
-    route_exposure = generate_route(coords, threshold)
-    normal_exposure = generate_normal(coords, threshold)
-    print(route_exposure, normal_exposure, "route exposure, normal exposure")
+    route_exposure, normal_exposure = generate_route(coords, threshold)
     old_route_exp, old_normal_exp = route_exposure, normal_exposure
+    print(route_exposure, normal_exposure, "route exposure, normal exposure")
+
     i = max_AQI - 1
     while i > 0:
         threshold = i
         print("threshold: "+str(threshold))
         polygonize(threshold, date_time)
-        max_poly = filter(threshold, date_time)
-        route_exposure = generate_route(coords, threshold)
-        sleep(5)
-        normal_exposure = generate_normal(coords, threshold)
+        exclude_poly = filter(threshold, date_time)
+        '''if max_poly != old_max_poly:
+            print(max_poly)'''
+        route_exposure, normal_exposure = generate_route(coords, threshold)
         if route_exposure != old_route_exp or normal_exposure != old_normal_exp:
             print(route_exposure, normal_exposure, "route exposure, normal exposure")
             i -= 1
--- ph_routing.py
+++ ph_routing.py
@@ -7,58 +7,75 @@ import base64
 from time import sleep
 from github import Github
 
-def calculate(result, threshold):
-    polyline = result.raw["trip"]["legs"][0]["shape"]
-    decoded = decode_polyline6(polyline)
-    route_points = [list(element) for element in decoded]
-    aqi = []
-    temp = []
-    total = 0
-    with open("./temp/polygonized"+str(threshold)+".json") as f:
-        data = json.load(f)
-    data['features'] = sorted(data['features'], key=lambda x: x["properties"]["AQI"], reverse=True)
-    points = [Point(i[0], i[1]) for i in route_points]
-    for polygon in data['features']:
-        if polygon["properties"]["AQI"] <= 500:
-            coordinates = polygon["geometry"]
-            temp.append([shape(coordinates),polygon["properties"]["AQI"]])
-    for i in points:
-        for j in temp:
-            if j[0].contains(i):
-                aqi.append([j,i])
-                break
-    x_distance = 0
-    y_distance = 0
-    total_distest = 0
-    for i in range(len(aqi)-1):
-        x_distance += int(110.574)*(aqi[i][1].x-aqi[i+1][1].x)
-        y_distance += int(111.320)*math.cos((aqi[i][1].y+aqi[i+1][1].y)/2)*(aqi[i][1].y-aqi[i+1][1].y)
-        level = aqi[i][0][1]
-        distance = math.sqrt((abs(aqi[i][1].x-aqi[i+1][1].x))**2+(abs(aqi[i][1].y-aqi[i+1][1].y))**2)
-        total += distance*level
-        total_distest += distance
-    return total/total_distest
-
 def generate_route(coords, threshold):
     client = Valhalla(base_url='https://valhalla1.openstreetmap.de')
-
+    
     with open("./temp/filtered"+str(threshold)+".json","r") as f:
         data = json.load(f)
         exclude_poly = data["features"][0]["geometry"]["coordinates"]
-        print(exclude_poly)
-        if exclude_poly == [[[]]]:
-            route = client.directions(locations=coords,instructions=True,profile="pedestrian")
-        else:
-            route = client.directions(locations=coords,instructions=True,profile="pedestrian",avoid_polygons=exclude_poly)
-    route = client.directions(locations=coords,instructions=True,profile="pedestrian",avoid_polygons=exclude_poly)
+
+    output_list = []
+    normal = client.directions(locations=coords,instructions=True,profile="pedestrian")
+    sleep(5)
+    if len(exclude_poly[0][0]):
+        route = client.directions(locations=coords,instructions=True,profile="pedestrian",avoid_polygons=exclude_poly)
+    else:
+        route = normal
+        
+    listy = [route, normal]
     output_dict = {"type": "FeatureCollection", "name": "filtered_output", "threshold": threshold, "features": [{"type": "Feature", "properties":{}, "geometry": {"type": "Polygon","coordinates": exclude_poly}},{"type": "Feature", "properties":{}, "geometry": {"type": "Point","coordinates": coords[0]}},{"type": "Feature", "properties":{}, "geometry": {"type": "Point","coordinates": coords[1]}}]}
-    json_output = json.dumps(output_dict, indent=4)
+    normal_output = json.dumps(normal.raw, indent=4)
+    with open("./results/normal_results"+str(threshold)+".json","w") as f:
+        f.write(normal_output)
+    
     route_output = json.dumps(route.raw, indent=4)
+    with open("./results/route_results"+str(threshold)+".json","w") as f:
+        f.write(route_output)
+    
+    for selector in listy:
+        polyline = selector.raw["trip"]["legs"][0]["shape"]
+        decoded = decode_polyline6(polyline)
+        route_points = [list(element) for element in decoded]
+        output_dict['features'].append({"type": "Feature", "properties":{}, "geometry": {"type": "LineString","coordinates": route_points}})
+        aqi = []
+        temp = []
+        total = 0
+        with open("./temp/polygonized"+str(threshold)+".json") as f:
+            data = json.load(f)
+        data['features'] = sorted(data['features'], key=lambda x: x["properties"]["AQI"], reverse=True)
+        points = [Point(i[0], i[1]) for i in route_points]
+        for polygon in data['features']:
+            if polygon["properties"]["AQI"] <= 500:
+                coordinates = polygon["geometry"]
+                temp.append([shape(coordinates),polygon["properties"]["AQI"]])
+        for i in points:
+            for j in temp:
+                if j[0].contains(i):
+                    aqi.append([j,i])
+                    break
+        x_distance = 0
+        y_distance = 0
+        total_distest = 0
+        for i in range(len(aqi)-1):
+            x_distance += int(110.574)*(aqi[i][1].x-aqi[i+1][1].x)
+            y_distance += int(111.320)*math.cos((aqi[i][1].y+aqi[i+1][1].y)/2)*(aqi[i][1].y-aqi[i+1][1].y)
+            level = aqi[i][0][1]
+            distance = math.sqrt((abs(aqi[i][1].x-aqi[i+1][1].x))**2+(abs(aqi[i][1].y-aqi[i+1][1].y))**2)
+            total += distance*level
+            total_distest += distance
+        output_list.append(total/total_distest)
+    
+    output = json.dumps(output_dict, indent=4)
+    '''with open("./results/results_"+str(threshold)+".json","w") as f:
+        f.write(output)'''
+        
     coded_string = "Z2hwXzY3emJ2MGpUdkZRVjdJR201ZXpNSWQ1dU5tOWFHRzNiakp3Tg=="
     g = Github(base64.b64decode(coded_string).decode("utf-8"))
     repo = g.get_repo("pctiope/heroku-python-script")
     contents = repo.get_contents("/geojson/route.geojson", ref="dev")
-    repo.update_file(contents.path, "updated route.geojson", json_output, contents.sha, branch="dev")
+    repo.update_file(contents.path, "updated route.geojson", output, contents.sha, branch="dev")
+    contents = repo.get_contents("/results/normal_results.raw", ref="dev")
+    repo.update_file(contents.path, "updated normal_results.raw", normal_output, contents.sha, branch="dev")
     contents = repo.get_contents("/results/route_results.raw", ref="dev")
     repo.update_file(contents.path, "updated route_results.raw", route_output, contents.sha, branch="dev")
-    return calculate(route, threshold)
+    return output_list[0], output_list[1]
