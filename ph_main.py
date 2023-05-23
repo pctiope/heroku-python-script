340ee397e2cf2cd3664db6f1a8b8c034b8e7866f
pctiope <pctiope@up.edu.ph>
2023-05-23 21:16:07 +0800

    fix to top 1 for now

--- ph_main.py
+++ ph_main.py
@@ -43,7 +43,7 @@ while 1:
     for i in range(len(Sensor_Name)):
          sensors.append(Sensor(Sensor_Name[i],X_location[i],Y_location[i],US_AQI[i]))
     sensors = sorted(sensors, key=lambda x: x.aqi, reverse=True)
-    top5_rand = random.randint(0,4)
+    top5_rand = random.randint(0,0)
     first_point, second_point = random_waypoints(poly, sensors[top5_rand].x, sensors[top5_rand].y)
 
     coords = [[first_point.x, first_point.y], [second_point.x, second_point.y]]
