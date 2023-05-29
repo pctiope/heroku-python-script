071ab9105d7942d495e463b9e9658f99745d631a
pctiope <pctiope@up.edu.ph>
2023-05-29 15:00:22 +0800

    change to float

--- ph_random.py
+++ ph_random.py
@@ -5,9 +5,9 @@ import math
 import random
 
 def random_waypoints(poly,Xo,Yo):
-    d = random.randrange(0.0025, 0.01, 0.0005)
+    d = float(random.randrange(25, 100, 5)/100)
     while True:
-        print('calculating')
+        print(d, 'calculating')
         rad = 2*math.pi*random.uniform(0,1)
         p1 = Point(Xo+d*math.cos(rad),Yo+d*math.sin(rad))
         p2 = Point(Xo-d*math.cos(rad),Yo-d*math.sin(rad))
