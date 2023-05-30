a258412598ac254f0114350c670e7fa4f3c27115
pctiope <pctiope@up.edu.ph>
2023-05-30 15:44:20 +0800

    actual working local setup

--- ph_initial.py
+++ ph_initial.py
@@ -1,8 +1,8 @@
 import json
 
 def export_initial(date_time, df, rmse_list):
-    json_output = df.to_json(orient='index')
-    json_output.append({"type": dict(rmse_list)})
+    json_output = df.to_dict(orient='index')
+    json_output['power and rmse'] = dict(rmse_list)
     json_output = json.dumps(json_output, indent=4)
     with open("./initial/initial_"+str(date_time)+".json","w") as f:
         f.write(json_output)
