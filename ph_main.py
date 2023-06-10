dc872b1026bb955f1a17eff58e9cad59d22951fb
pctiope <pctiope@up.edu.ph>
2023-06-10 16:17:45 +0800

    added loops

--- ph_main.py
+++ ph_main.py
@@ -25,29 +25,30 @@ path = "./results/"
 os.makedirs(path, exist_ok=True)
 print(f"Directory '{path}' created successfully")
 
-# get AQI sensor data
-WAQI_sensors, IQAir_locations, IQAir_sensors = init_sensors()
-Sensor_Name, X_location, Y_location, US_AQI, df = get_sensor_data(WAQI_sensors, IQAir_locations, IQAir_sensors)
-df_to_shp(df)
+while 1:
+    # get AQI sensor data
+    WAQI_sensors, IQAir_locations, IQAir_sensors = init_sensors()
+    Sensor_Name, X_location, Y_location, US_AQI, df = get_sensor_data(WAQI_sensors, IQAir_locations, IQAir_sensors)
+    df_to_shp(df)
 
-# IDW methods
-get_idw(2)
+    # IDW methods
+    get_idw(2)
 
-# get border polygon
-with open("./metro_manila.geojson") as f:
-    data = json.load(f)
-    border_poly = Polygon(data['features'][0]['geometry']['coordinates'][0][0])
-sensors = [
-    AQI_Sensor(Sensor_Name[i], X_location[i], Y_location[i], US_AQI[i])
-    for i in range(len(Sensor_Name))
-]
-sensors = sorted(sensors, key=lambda x: x.aqi, reverse=True)
-# print(sensors)
+    # get border polygon
+    with open("./metro_manila.geojson") as f:
+        data = json.load(f)
+        border_poly = Polygon(data['features'][0]['geometry']['coordinates'][0][0])
+    sensors = [
+        AQI_Sensor(Sensor_Name[i], X_location[i], Y_location[i], US_AQI[i])
+        for i in range(len(Sensor_Name))
+    ]
+    sensors = sorted(sensors, key=lambda x: x.aqi, reverse=True)
+    # print(sensors)
 
-# polygonize and filter
-max_AQI = max(int(i) for i in US_AQI)
-threshold = max_AQI*random.randint(55, 95)/100
-print(max_AQI, threshold)
-polygonize()
-filter(threshold, border_poly)
-sleep(30)
+    # polygonize and filter
+    max_AQI = max(int(i) for i in US_AQI)
+    threshold = max_AQI*random.randint(55, 95)/100
+    print(max_AQI, threshold)
+    polygonize()
+    filter(threshold, border_poly)
+    sleep(30)
